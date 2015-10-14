e1d15218158e2da502fd9a68b0d40470eda27dfd
bkate <kaoutare.bedda@haw-hamburg.de>
2015-10-14 17:59:07 +0200

    @jriehn @bkate EMZ-001 initialize backen +gradle  spring boot

---
+++ src/main/java/com/emanzel/controller/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.emanzel.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @RequestMapping("/")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+}

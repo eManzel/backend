963c0d4019ceb64a505f91e8bf4c950ac415af64
bkate <kaoutare.bedda@haw-hamburg.de>
2015-10-15 16:56:45 +0200

    @jriehn @bkate add IT

--- src/test/java/com/emanzel/controller/HelloWorldIT.java
+++ src/test/java/com/emanzel/controller/HelloWorldIT.java
@@ -1,6 +1,34 @@
 package com.emanzel.controller;
 
+import com.emanzel.Application;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {Application.class})
 public class HelloWorldIT {
 
+    @Autowired
+    private HelloWorld controller;
 
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void shouldReturnHelloWorld() throws Exception {
+        mockMvc.perform(get("/hello")).andExpect(status().isOk());
+    }
 }
